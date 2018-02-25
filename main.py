2497c743accaa87eafbc72deaccba14e26b9bdb6
EmmaBR <emmabr.626@gmail.com>
2018-02-25 10:20:12 -0800

    Create main.py
    Incomplete code

---
+++ main.py
@@ -0,0 +1,54 @@
+#python code for twilio response
+import requests
+from twilio.rest import Client
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+account_sid = "AC75325c6ca65ba0dc7e86b5b6a75eaf99"
+auth_token = "58d18c8197a684de3826a1487aebc95c"
+client = Client(account_sid, auth_token)
+
+rawData = requests.get()
+userData = requests.get()
+
+exists = False
+alreadyCheckedIn = False
+
+txtFile = open('schedule.txt')
+
+for i in userData:
+    if i['person_id'] == rawData['person_id']:
+        exists = True
+        phoneNum = rawData['phone']
+        #Hi there first name
+    else:
+        exists = False
+        #Create an account
+
+if exists:
+    if rawData['checkIn'] == 'true':
+        alreadyCheckedIn = True
+    else:
+        for i in userData:
+            if i['person_id'] == rawData['person_id']:
+                userData['checkIn'] = "true"
+
+if (exists == True) and (alreadyCheckedIn == False):
+    outputTxt = "You've successfully checked in!"
+    schedule = testFile.read()
+elif alreadyCheckedIn:
+    outputTxt = "You have already checked in! Would you like the schedule again? (Y/N)"
+    if request.form['body'] == "Y":
+        schedule = testFile.read()
+    else:
+        schedule = ''
+        pass
+else:
+    outputTxt = "You've Successfully created an account and checked in!"
+    schedule = testFile.read()
+
+client.messages.create(
+    to=phoneNum,
+    from_="+19093231924",
+    body= outputTxt + '\n' + schedule,
+)
