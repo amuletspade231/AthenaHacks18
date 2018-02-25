db5df271ac5b89d8c26c4bcf456b1defb320d8bc
amuletspade231 <anniemaroon@gmail.com>
2018-02-25 04:02:49 -0800

    Camera Capture Iteration 1

---
+++ MainPage.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.ApplicationModel;
+using System.Threading.Tasks;
+using Windows.System.Display;
+using Windows.Graphics.Display;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Storage.FileProperties;
+using Windows.Graphics.Imaging;
+using Windows.Media.MediaProperties;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CameraCapture
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture mediaCapture;
+        bool isPreviewing;
+        DisplayRequest displayRequest = new DisplayRequest();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Application.Current.Suspending += Application_Suspending;
+        }
+
+        //-----------Helper Functions------------
+
+        private async Task CapturePhotoToFile()
+        {
+            mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+            //mediaCapture.Failed += MediaCapture_Failed;
+
+            // Prepare and capture photo
+            var lowLagCapture = await mediaCapture.PrepareLowLagPhotoCaptureAsync(ImageEncodingProperties.CreateUncompressed(MediaPixelFormat.Bgra8));
+
+            var capturedPhoto = await lowLagCapture.CaptureAsync();
+            var softwareBitmap = capturedPhoto.Frame.SoftwareBitmap;
+
+            await lowLagCapture.FinishAsync();
+
+            StorageFolder destinationFolder =
+                await ApplicationData.Current.LocalFolder.CreateFolderAsync("ProfilePhotoFolder",
+                CreationCollisionOption.OpenIfExists);
+            
+            StorageFile file = await destinationFolder.CreateFileAsync("photo.jpg", CreationCollisionOption.GenerateUniqueName);
+
+            using (var captureStream = new InMemoryRandomAccessStream())
+            {
+                await mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), captureStream);
+
+                using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var decoder = await BitmapDecoder.CreateAsync(captureStream);
+                    var encoder = await BitmapEncoder.CreateForTranscodingAsync(fileStream, decoder);
+
+                    var properties = new BitmapPropertySet {
+            { "System.Photo.Orientation", new BitmapTypedValue(PhotoOrientation.Normal, PropertyType.UInt16) }
+        };
+                    await encoder.BitmapProperties.SetPropertiesAsync(properties);
+
+                    await encoder.FlushAsync();
+                }
+            }
+        }
+
+        //Depending on your app's scenario, you may want to call this from the OnNavigatedTo event handler that is called when the page is loaded
+        //or wait and launch the preview in response to UI events
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This will be thrown if the user denied access to the camera in privacy settings
+                // ShowMessageToUser("The app was denied access to the camera");
+                var messageDialog = new MessageDialog("The app was denied access to the camera.");
+                return;
+            }
+
+            try
+            {
+                PreviewControl.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isPreviewing = true;
+            }
+            catch (System.IO.FileLoadException)
+            {
+                mediaCapture.CaptureDeviceExclusiveControlStatusChanged += _mediaCapture_CaptureDeviceExclusiveControlStatusChanged;
+            }
+
+        }
+
+        private async void _mediaCapture_CaptureDeviceExclusiveControlStatusChanged(MediaCapture sender, MediaCaptureDeviceExclusiveControlStatusChangedEventArgs args)
+        {
+            if (args.Status == MediaCaptureDeviceExclusiveControlStatus.SharedReadOnlyAvailable)
+            {
+                var messageDialog = new MessageDialog("The camera preview can't be displayed because another app has exclusive access");
+            }
+            else if (args.Status == MediaCaptureDeviceExclusiveControlStatus.ExclusiveControlAvailable && !isPreviewing)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    await StartPreviewAsync();
+                });
+            }
+        }
+
+        //public delegate void MediaCaptureFailedEventHandler(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs);
+        //public sealed class MediaCaptureFailedEventArgs : IMediaCaptureFailedEventArgs
+        //public event MediaCaptureFailedEventHandler Failed<>
+
+        private async Task CleanupCameraAsync()
+        {
+            if (mediaCapture != null)
+            {
+                if (isPreviewing)
+                {
+                    await mediaCapture.StopPreviewAsync();
+                }
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    PreviewControl.Source = null;
+                    if (displayRequest != null)
+                    {
+                        displayRequest.RequestRelease();
+                    }
+
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                });
+            }
+
+        }
+
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            await CleanupCameraAsync();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await StartPreviewAsync();
+        }
+
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                await CleanupCameraAsync();
+                deferral.Complete();
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await CapturePhotoToFile();
+        }
+    }
+}
